7e7d7c61da9e33838fe1af712481b126b8423235
kiki-kanri <a470666@gmail.com>
2025-01-13 10:20:54 +0800

    feat: implement `AsRef`, `Clone`, `Display`, `Eq`, and `PartialEq` traits for path struct

--- src/path.rs
+++ src/path.rs
@@ -1,4 +1,5 @@
 use std::borrow::Cow;
+use std::fmt::{Display, Formatter};
 use std::path::{Path as StdPath, PathBuf};
 
 #[derive(Debug)]
@@ -19,5 +20,32 @@ impl Path {
 
     pub fn to_string_lossy(&self) -> Cow<'_, str> {
         return self.path.to_string_lossy();
+    }
+}
+
+impl AsRef<StdPath> for Path {
+    fn as_ref(&self) -> &StdPath {
+        return &self.path;
+    }
+}
+
+impl Clone for Path {
+    fn clone(&self) -> Self {
+        return Path {
+            path: self.path.clone(),
+        };
+    }
+}
+
+impl Display for Path {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        return write!(f, "{}", self.path.to_string_lossy());
+    }
+}
+
+impl Eq for Path {}
+impl PartialEq for Path {
+    fn eq(&self, other: &Self) -> bool {
+        return self.path == other.path;
     }
 }
