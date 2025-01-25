78efe5b06faa5374d7191265081036afcaa1b729
kiki-kanri <a470666@gmail.com>
2025-01-25 14:58:34 +0800

    test: change test functions name in `core.rs`

--- tests/core.rs
+++ tests/core.rs
@@ -3,20 +3,20 @@ use pathkit::Path;
 use std::ffi::OsStr;
 
 #[test]
-fn new_creates_path_instance() {
+fn new_instance() {
     let path = Path::new("/test/path");
     assert_eq!(path.to_str(), Some("/test/path"));
 }
 
 #[test]
-fn absolutize_resolves_relative_path() -> Result<()> {
+fn absolutize() -> Result<()> {
     let path = Path::new(".");
     assert!(path.absolutize()?.is_absolute());
     return Ok(());
 }
 
 #[test]
-fn absolutize_from_uses_specified_cwd() -> Result<()> {
+fn absolutize_from() -> Result<()> {
     let path = Path::new("subdir");
     assert_eq!(
         path.absolutize_from("/base")?.to_str(),
@@ -27,7 +27,7 @@ fn absolutize_from_uses_specified_cwd() -> Result<()> {
 }
 
 #[test]
-fn absolutize_virtually_uses_virtual_root() -> Result<()> {
+fn absolutize_virtually() -> Result<()> {
     let path = Path::new("subdir/file.txt");
     assert_eq!(
         path.absolutize_virtually("/virtual")?.to_str(),
@@ -38,32 +38,32 @@ fn absolutize_virtually_uses_virtual_root() -> Result<()> {
 }
 
 #[test]
-fn canonicalize_returns_absolute_path() -> Result<()> {
+fn canonicalize() -> Result<()> {
     let path = Path::new(".");
     assert!(path.canonicalize()?.is_absolute());
     return Ok(());
 }
 
 #[test]
-fn extension_returns_file_extension() {
+fn extension() {
     let path = Path::new("file.txt");
     assert_eq!(path.extension(), Some(OsStr::new("txt")));
 }
 
 #[test]
-fn file_name_returns_last_segment() {
+fn file_name() {
     let path = Path::new("/path/to/file.txt");
     assert_eq!(path.file_name(), Some(OsStr::new("file.txt")));
 }
 
 #[test]
-fn file_stem_returns_file_name_without_extension() {
+fn file_stem() {
     let path = Path::new("file.txt");
     assert_eq!(path.file_stem(), Some(OsStr::new("file")));
 }
 
 #[test]
-fn is_absolute_checks_if_path_is_absolute() {
+fn is_absolute() {
     let path = Path::new("/absolute/path");
     assert!(path.is_absolute());
     let relative_path = Path::new("relative/path");
@@ -71,25 +71,25 @@ fn is_absolute_checks_if_path_is_absolute() {
 }
 
 #[test]
-fn join_combines_paths() {
+fn join() {
     let path = Path::new("/base");
     assert_eq!(path.join("subdir").to_str(), Some("/base/subdir"));
 }
 
 #[test]
-fn parent_returns_parent_directory() {
+fn parent() {
     let path = Path::new("/base/subdir/file.txt");
     assert_eq!(path.parent().unwrap().to_str(), Some("/base/subdir"));
 }
 
 #[test]
-fn to_str_converts_path_to_str() {
+fn to_str() {
     let path = Path::new("/test/path");
     assert_eq!(path.to_str(), Some("/test/path"));
 }
 
 #[test]
-fn to_string_lossy_returns_lossy_string() {
+fn to_string_lossy() {
     let path = Path::new("/test/path");
     assert_eq!(path.to_string_lossy(), "/test/path");
 }
