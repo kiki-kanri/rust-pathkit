ad5c35fd47dc4486422e039ec4d158a568eb0919
kiki-kanri <a470666@gmail.com>
2025-05-01 09:22:24 +0800

    chore: update `.gitignore` and `modify-files-permissions.sh`

--- modify-files-permissions.sh
+++ modify-files-permissions.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+cd "$(realpath "$(dirname "$(readlink -f "$0")")")" || exit 1
+
 git config --replace-all core.filemode true
-find . -name target -prune -o -type f -exec chmod 600 {} +
-find . -name target -prune -o -type d -exec chmod 700 {} +
-find . -name target -prune -o -name '*.sh' -type f -exec chmod 700 {} +
+find . -name target -prune -o \( -type f -exec chmod 600 {} + \)
+find . -name target -prune -o \( -type d -exec chmod 700 {} + \)
+find . -name target -prune -o \( -name '*.sh' -type f -exec chmod 700 {} + \)
