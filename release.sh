64d18747122bde79e27e3aeab2b3c665d56f42d1
kiki-kanri <a470666@gmail.com>
2025-05-31 20:18:58 +0800

    chore: update scripts

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+
 if ! git diff-index --quiet HEAD --; then
     echo 'Error: There are uncommitted changes in your working directory.'
     echo 'Please commit or discard the changes before proceeding.'
@@ -8,9 +9,11 @@ if ! git diff-index --quiet HEAD --; then
 fi
 
 cargo test
+
 pnpx @kikiutils/changelogen --bump
 new_version=$(node -p "require('./package.json').version")
 cargo set-version "$new_version"
+
 git checkout -- ./CHANGELOG.md ./package.json
 git add ./Cargo.toml
 pnpx @kikiutils/changelogen --push --release
