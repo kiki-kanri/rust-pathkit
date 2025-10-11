8c85f4f970f20dace1f26170cb9972f44be56aad
kiki-kanri <a470666@gmail.com>
2025-10-12 02:51:04 +0800

    chore: update release script

--- release.sh
+++ release.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
+
+SCRIPTS_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+cd "${SCRIPTS_DIR}"
 
 if ! git diff-index --quiet HEAD --; then
-    echo 'Error: There are uncommitted changes in your working directory.'
-    echo 'Please commit or discard the changes before proceeding.'
+    echo 'Error: There are uncommitted changes in your working directory'
+    echo 'Please commit or discard the changes before proceeding'
     exit 1
 fi
 
-cargo test
+cargo format
+cargo lint
+cargo t --all-features
+cargo b -r --all-features
 
-pnpx @kikiutils/changelogen --bump
+pnpx changelogen@latest --bump --hideAuthorEmail
 new_version=$(node -p "require('./package.json').version")
 cargo set-version "$new_version"
-
 git checkout -- ./CHANGELOG.md ./package.json
 git add ./Cargo.toml
-pnpx @kikiutils/changelogen --push --release
+pnpx changelogen@latest --hideAuthorEmail --push --release
 cargo publish
