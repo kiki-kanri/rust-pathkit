122928d1faaf398e8ead2eb0d82574199193c9b9
kiki-kanri <a470666@gmail.com>
2025-10-12 15:36:21 +0800

    refactor!: make `AsyncFsOps` enabled via feature flag

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#[cfg(feature = "async-fs-ops")]
 mod async_fs_ops;
 mod core;
 mod div;
@@ -6,5 +7,6 @@ mod traits;
 
 pub use core::Path;
 
+#[cfg(feature = "async-fs-ops")]
 pub use async_fs_ops::AsyncFsOps;
 pub use sync_fs_ops::SyncFsOps;
